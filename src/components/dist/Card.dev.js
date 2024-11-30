d00987e12cf443491fa1f2a22bb38594e8154253
arpita-32 <arpitasundaray9@gmail.com>
2024-11-30 12:44:14 +0530

    first commit

---
+++ src/components/dist/Card.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function Card() {
+  return m;
+}
+
+var _default = Card;
+exports["default"] = _default;
+//# sourceMappingURL=Card.dev.js.map
---
+++ src/components/dist/Tours.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function Tours() {
+  return;
+}
+
+var _default = Tours;
+exports["default"] = _default;
+//# sourceMappingURL=Tours.dev.js.map
